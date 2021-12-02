d9a11ab8358eed6f518286f210dc46d67934eeb7
Ethan <44066853+EthanSantos@users.noreply.github.com>
2021-12-02 01:02:00 -0800

    main

--- Labs/Lab5B.cpp
+++ Labs/Lab5B.cpp
@@ -1,42 +1,59 @@
 /*
 Prolog
-a.  Program Description: Program that takes a user's inputs for an array and prints their array with the elements in reversed order.
+a.  Program Description: Program that takes a user's inputs for two arrays and combines the two array's elements and displays it
 b.  Author: Ethan Santos
-c.  Date: November 30, 2021
+c.  Date: December 2, 2021
 d.  Input variables: "input" float variable that is stored into an array
-e.  Process Flow: Program asks the user to input values into an array. The program displays their array then uses the reverse() function to reverse the order of the
-array and display that back to the user. The reverse function works by using pointers and looping the arrays elements backwards.
-f.  Output variables: int variable (returns 0 in the main() function)
+e.  Process Flow: Program takes the user's input for two arrays using the read_data function which uses pointers to manipulate and add elements
+to the array. The program then prints back the entire array. After creating the 2 arrays, the program combines both arrays into one new array 
+using for loops and displays the new array.
+f.  Output variables: pointer int variable (size)
 */
 
 #include <iostream>
-
 using namespace std;
- 
-void reverse(double* a, int size){
-    double* p = a;
-    p += size-1; // set to last element of array
+
+int* read_data(float* array, int& size){
+    float input;
+    float* p = array;
+    cout << endl << "Enter values for the array and press Q/q to stop: ";
+    while (cin >> input){
+        array[size] = input;
+        size++;
+    }
+    return &size;
+}
+
+void display_array(string message, float array[], int size){
+    cout << message;
     for (int i = 0; i < size; i++){
-        cout << *p << " ";;
-        p--;
+        cout << array[i] << " ";
     }
 }
 
 int main(){
     const int CAPACITY = 20;
-    double input;
-    int size = 0;
-    double array[CAPACITY];
-    cout << "Enter the values for the array and press Q/q to stop: ";
-    while (cin >> input){
-        array[size] = input;
-        size++;
+    int first_size = 0;
+    int second_size = 0;
+    float first_array[CAPACITY];
+    float second_array[CAPACITY];
+    float combined_array[CAPACITY];
+    read_data(first_array, first_size);
+    display_array("First array: ", first_array, first_size);
+    cin.clear();
+    cin.ignore();
+    read_data(second_array, second_size);
+    display_array("Second array: ", second_array, second_size);
+    // create new array
+    int total_size = second_size + first_size;
+    cout << endl << "New combined size: " << (total_size) << endl;
+    for (int i = 0; i < first_size; i++){
+        combined_array[i] = first_array[i];
     }
-    cout << "Your array: ";
-    for (int i = 0; i < size; i++){
-        cout << array[i] << " ";
+    for (int i = 0; i < second_size; i++){
+        combined_array[i+first_size] = second_array[i];
     }
-    cout << endl << "Your array reversed: ";
-    reverse(array, size);
+    display_array("New combined array: ", combined_array, total_size);
     return 0;
 }
+
